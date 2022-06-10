0e744e0a3015568319b99c9fe3c5c12c3ec84760
Francis Williams <franciswilliams1999@gmail.com>
2022-06-09 23:41:07 -0500

    changed values of enum flips

--- src/Vector2d.hpp
+++ src/Vector2d.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-enum Flips{FORWARD, BACKWARD};
+enum Flips{FORWARD = 1, BACKWARD = -1};
 enum pivots{CENTER = 0, TOP_LEFT, CENTER_LEFT, BOTTOM_LEFT, TOP_CENTER, BOTTOM_CENTER, TOP_RIGHT, CENTER_RIGHT, BOTTOM_RIGHT};
 struct Vector2d {
     private:
@@ -9,7 +9,7 @@ struct Vector2d {
     constexpr static float grav = 70.0f;
     short x_i=0, y_i=0, x=0, y=0, vel_x=0, vel_y=0, acc_x=0, acc_y=0;
     unsigned int t0_x = 0, t0_y = 0; // t0 for velocity and gravit calculations
-    bool direction;
+    Flips direction;
     pivots pivot = CENTER;
 
     Vector2d() {
